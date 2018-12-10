718323cdbc1a2e5377160f0cd1cdfcdca6632df1
Yousof Mersal <yousof777@gmail.com>
2018-12-10 17:30:20 +0100

    edit README and support .env

--- src/server/server.js
+++ src/server/server.js
@@ -6,6 +6,11 @@ const app = express()
 const buildPath = path.join(__dirname, '../../build')
 app.use(express.static(buildPath))
 
+app.get('/*', (req, res) => {
+  const indexPath = path.join(buildPath, 'index.html')
+  res.sendFile(indexPath)
+})
+
 app.listen(port, () => {
   console.log(`app being served on port ${port}`)
 })
