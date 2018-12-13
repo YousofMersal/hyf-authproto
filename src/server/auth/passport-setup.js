89c10b58e5445a58103e0a1ed6423440a4dc6533
Yousof Mersal <yousof777@gmail.com>
2018-12-14 00:43:18 +0100

    change to database pooling instead of maneging connections mannually

--- src/server/auth/passport-setup.js
+++ src/server/auth/passport-setup.js
@@ -1,5 +1,13 @@
 const mysql = require('mysql')
-const connection = mysql.createConnection(process.env.JAWSDB_URL)
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+})
+// const connection = mysql.createConnection(process.env.JAWSDB_URL)
 const passport = require('passport')
 const GitHubStrategy = require('passport-github').Strategy
 
@@ -8,8 +16,7 @@ passport.serializeUser((user, done) => {
 })
 
 passport.deserializeUser((id, done) => {
-  connection.connect()
-  connection.query(
+  pool.query(
     'select * FROM users where github_id = ?',
     [id],
     (err, results, fields) => {
@@ -32,15 +39,14 @@ passport.use(
     },
     (accessToken, refreshToken, profile, done) => {
       //passport callbaack
-      connection.connect()
-      connection.query(
+      pool.query(
         'select * FROM users WHERE github_id = ?',
         [profile._json.id],
         (err, results, fields) => {
           if (err) {
             throw new Error('Something went wrong in fething a user!' + err)
           } else if (results === undefined || results.length === 0) {
-            connection.query(
+            pool.query(
               'INSERT INTO users (github_id, name, github_login, type, avatar) VALUES( ?, ?, ?, ?, ?)',
               [
                 profile._json.id,
