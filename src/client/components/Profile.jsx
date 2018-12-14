66230107210e5f7e26c408a2d4a75ec8841cc72c
Yousof Mersal <yousof777@gmail.com>
2018-12-14 17:03:04 +0100

    add conditional rendering to profile page

--- src/client/components/Profile.jsx
+++ src/client/components/Profile.jsx
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 
 export default class Profile extends Component {
   render() {
-    console.log(this.props.user)
-    return (
-      <div className="userinfo">
-        <p> Welcome back {this.props.user.name}!</p>
-        <img src={this.props.user.avatar} alt="none available" />
-      </div>
-    )
+    if (Object.keys(this.props.user).length > 1) {
+      return (
+        <div className="userinfo">
+          <p> Welcome back {this.props.user.name}!</p>
+          <img src={this.props.user.avatar} alt="none available" />
+        </div>
+      )
+    } else {
+      return <h1>Please log in to see your profile information</h1>
+    }
   }
 }
